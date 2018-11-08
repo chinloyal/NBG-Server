ceccb2fcdbc7d95f8067fd1a2c3eb1e3bd8c7023
BritAndy12 <brittaney.anderson@hotmail.com>
2018-11-07 22:18:17 -0500

    Attempted to create and filter transactions.

--- Server/database/UserProvider.java
+++ Server/database/UserProvider.java
@@ -15,7 +15,7 @@ import models.User;
 
 public class UserProvider extends SQLProvider<User> {
 	private static final String TABLE_NAME = "users";
-	//Implement a logger for every provider
+	// Implement a logger for every provider
 	private static Logger logger = LogManager.getLogger(UserProvider.class);
 
 	public UserProvider() {
@@ -24,8 +24,7 @@ public class UserProvider extends SQLProvider<User> {
 
 	@Override
 	protected void initDatabase() {
-		
-		
+
 	}
 
 	@Override
@@ -33,39 +32,61 @@ public class UserProvider extends SQLProvider<User> {
 		List<User> users = null;
 		try {
 			users = new ArrayList<User>();
-			
+
 			String query = "SELECT * FROM " + TABLE_NAME;
 			statement = connection.createStatement();
-			
+
 			logger.info("Executing " + query);
-			
+
 			resultSet = statement.executeQuery(query);
-			
-			while(resultSet.next()) {
-				User user = new User(
-					resultSet.getInt("id"),
-					resultSet.getString("first_name"),
-					resultSet.getString("last_name"),
-					resultSet.getString("type"),
-					resultSet.getString("email"),
-					resultSet.getString("password")
-				);
-				
+
+			while (resultSet.next()) {
+				User user = new User(resultSet.getInt("id"), resultSet.getString("first_name"),
+						resultSet.getString("last_name"), resultSet.getString("type"), resultSet.getString("email"),
+						resultSet.getString("password"));
+
 				users.add(user);
 			}
-			
+
 			logger.debug("Retrieved " + users.size() + " user(s).");
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			logger.error("Failed to execute select all query for Table: " + TABLE_NAME);
 		}
-		
+
 		return users;
 	}
 
 	@Override
 	public User get(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = null;
+
+		try {
+			user = new User();
+
+			String query = "SELECT * FROM " + TABLE_NAME + " WHERE id = ?";
+			logger.debug("Retrieving Customer with ID: " +id);
+
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, id);
+			resultSet = preparedStatement.executeQuery();
+
+			if (resultSet.next()) {
+				User temp = new User(
+						resultSet.getInt("id"),
+						resultSet.getString("first_name"),
+						resultSet.getString("last_name"), 
+						resultSet.getString("type"), 
+						resultSet.getString("email"),
+						resultSet.getString("password"));
+				user = temp;
+			}
+
+			logger.debug("Retrieved Customer with ID: " +id);
+		} catch (SQLException e) {
+			logger.error("Failed to execute 'Get User By Id' query for Table: " + TABLE_NAME);
+		}
+
+		return user;
 	}
 
 	@Override
@@ -89,116 +110,110 @@ public class UserProvider extends SQLProvider<User> {
 	@Override
 	public int store(User user) {
 		try {
-			String query = "INSERT INTO " + TABLE_NAME + 
-							"(first_name, last_name, email, type, password) " +
-							"VALUES(?, ?, ?, ?, ?)";
+			String query = "INSERT INTO " + TABLE_NAME + "(first_name, last_name, email, type, password) "
+					+ "VALUES(?, ?, ?, ?, ?)";
 			preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-			
+
 			preparedStatement.setString(1, user.getFirstName());
 			preparedStatement.setString(2, user.getLastName());
 			preparedStatement.setString(3, user.getEmail());
 			preparedStatement.setString(4, user.getType());
 			preparedStatement.setString(5, BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
-			
+
 			int userRowsAffected = preparedStatement.executeUpdate();
-			
-			if(user.getType().equals("customer") && user.getPhoto() != null) {
+
+			if (user.getType().equals("customer") && user.getPhoto() != null) {
 				Photo profilePhoto = user.getPhoto();
 				int user_id = getLastInsertedId(preparedStatement);
-				
-				query = "INSERT INTO photos (file, user_id) "+
-						"VALUES ('"+ profilePhoto.getName() +"', "+ user_id +")";
+
+				query = "INSERT INTO photos (file, user_id) " + "VALUES ('" + profilePhoto.getName() + "', " + user_id
+						+ ")";
 				statement = connection.createStatement();
-				
+
 				statement.executeUpdate(query);
 			}
-			
+
 			return userRowsAffected;
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			logger.error("Failed to store user.", e.getMessage());
-//			e.printStackTrace();
+			// e.printStackTrace();
 		}
-		
+
 		return 0;
 	}
-	
+
 	public boolean authenticate(String email, String userPassword) {
 		try {
 			String query = "Select * from " + TABLE_NAME + " where email = ?";
-			logger.debug("Email: "+ email + "Password: "+ userPassword);
+			logger.debug("Email: " + email + "Password: " + userPassword);
 			preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setString(1, email);
 			resultSet = preparedStatement.executeQuery();
-			if(resultSet.next()) {
-				
+			if (resultSet.next()) {
+
 				return BCrypt.checkpw(userPassword, resultSet.getString("password"));
 			}
-			
+
 			return false;
-			
+
 		} catch (SQLException e) {
 			logger.error("Failed to get user credentials.", e.getMessage());
 			e.printStackTrace();
 		}
 		return false;
 	}
-	
+
 	public User getByEmail(String email) {
 		User user = null;
-		
+
 		try {
 			user = new User();
-			
+
 			String query = "SELECT * FROM " + TABLE_NAME + " WHERE email = ?";
-			logger.debug("Retrieving Customer with email: "+email);
-			
+			logger.debug("Retrieving Customer with email: " + email);
+
 			preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setString(1, email);
 			resultSet = preparedStatement.executeQuery();
-	
-			if(resultSet.next()) {
-				User temp = new User(
-					resultSet.getInt("id"),
-					resultSet.getString("first_name"),
-					resultSet.getString("last_name"),
-					resultSet.getString("type"),
-					resultSet.getString("email"),
-					resultSet.getString("password")
-				);
+
+			if (resultSet.next()) {
+				User temp = new User(resultSet.getInt("id"), resultSet.getString("first_name"),
+						resultSet.getString("last_name"), resultSet.getString("type"), resultSet.getString("email"),
+						resultSet.getString("password"));
 				user = temp;
 			}
-			
-			logger.debug("Retrieved customer with email: "+email);
-		}catch(SQLException e) {
+
+			logger.debug("Retrieved customer with email: " + email);
+		} catch (SQLException e) {
 			logger.error("Failed to execute 'Get User By Email' query for Table: " + TABLE_NAME);
 		}
-		
+
 		return user;
 	}
-	
+
 	public String getCusPhoto(User customer) {
 		String photo = null;
-		
+
 		try {
-			
+
 			String query = "SELECT * FROM photos WHERE user_id = ?";
-			logger.debug("Retrieving Photo with Customer ID: "+customer.getId());
-			
+			logger.debug("Retrieving Photo with Customer ID: " + customer.getId());
+
 			preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setInt(1, customer.getId());
-			
+
 			resultSet = preparedStatement.executeQuery();
-	
-			if(resultSet.next()) {
-				
+
+			if (resultSet.next()) {
+
 				photo = resultSet.getString("file");
 			}
-			
-			logger.debug("Retrieved photo with customer ID: "+customer.getId());
-		}catch(SQLException e) {
+
+			logger.debug("Retrieved photo with customer ID: " + customer.getId());
+		} catch (SQLException e) {
 			logger.error("Failed to execute 'Get Customer Photo' query for Table: " + TABLE_NAME);
 		}
-		
+
 		return photo;
 	}
 
